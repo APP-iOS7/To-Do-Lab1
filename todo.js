37c8d9f8bfe7c3fd56af429d2d3de067dc23674d
DeokWon Kim <khjs7878@naver.com>
2024-12-27 13:38:04 +0900

    dev반영

--- todo.js
+++ todo.js
@@ -4,15 +4,26 @@ const todoInput = document.getElementById('todoInput');
 
 function addTodo(text, checked = false) {
     
-   
+    const li = document.createElement('li');
+    li.classList.add(
+        'list-group-item',
+        'd-flex',
+        'align-items-center',
+        'justify-content-between'
+    );
+
     //텍스트 추가
     const spanElement = document.createElement('span');
     spanElement.classList.add('ms-2', 'flex-grow-1');
     spanElement.textContent = text;
 
     li.append(spanElement);
- 
+    todoListElement.appendChild(li);
 }
-   
+    addButton.addEventListener('click', () => {
+        if (todoInput.value.trim() === '') return;
+
+        addTodo(todoInput.value);
+    });
 
 
